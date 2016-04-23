26d13d7e8788607f70783edf83db3621b95ad0be
Christoffer Kjeldgaard Petersen <ckp@trifork.com>
2016-04-23 17:37:43 +0200

    Added Flow dependency

--- app/src/main/java/com/trifork/ckp/namequiz/MainActivity.java
+++ app/src/main/java/com/trifork/ckp/namequiz/MainActivity.java
@@ -1,7 +1,10 @@
 package com.trifork.ckp.namequiz;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import flow.Flow;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override protected void attachBaseContext(Context baseContext) {
+        baseContext = Flow.configure(baseContext, this).install();
+        super.attachBaseContext(baseContext);
     }
 }

1312e093faed15a6508ada44c5ca6c8365d5463a
Christoffer Kjeldgaard Petersen <ckp@trifork.com>
2016-04-25 15:23:55 +0200

    Added Espresso tests for default navigation with Flow

---
+++ app/src/androidTest/java/com/trifork/ckp/namequiz/NavigationTest.java
@@ -0,0 +1,25 @@
+package com.trifork.ckp.namequiz;
+
+import android.support.test.rule.ActivityTestRule;
+
+import com.trifork.ckp.namequiz.start.StartView;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+public class NavigationTest {
+
+    @Rule public ActivityTestRule rule = new ActivityTestRule<>(MainActivity.class);
+
+    /** Verifies that the app is in its default state on a cold start. */
+    @Test public void defaultKeyIsUsed() {
+        onView(withId(R.id.activity_main))
+                .check(matches(hasDescendant(isAssignableFrom(StartView.class))));
+    }
+}
--- app/src/main/java/com/trifork/ckp/namequiz/start/StartView.java
+++ app/src/main/java/com/trifork/ckp/namequiz/start/StartView.java
@@ -3,15 +3,23 @@ package com.trifork.ckp.namequiz.start;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.trifork.ckp.namequiz.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import flow.Flow;
 
-public class StartView extends LinearLayout {
+public class StartView extends RelativeLayout {
+
+    private Spinner departmentSpinner;
 
     public StartView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -19,6 +27,25 @@ public class StartView extends LinearLayout {
 
     @Override protected void onFinishInflate() {
         super.onFinishInflate();
+
+        departmentSpinner = (Spinner) findViewById(R.id.department_spinner);
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("København");
+        categories.add("Aarhus");
+        categories.add("Aalborg");
+        categories.add("Esbjerg");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        departmentSpinner.setAdapter(dataAdapter);
+
         /*EditText nameView = (EditText) findViewById(R.id.welcome_screen_name);
 
         nameView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
